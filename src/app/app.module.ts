20e175cc2826470cab3b9f0809bf23de09193137
Eero Lehtonen <eero.lehtonen@aalto.fi>
2017-11-25 14:34:48 +0200

    inicial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { MaterialsComponent } from './materials/materials.component';
+import { ManufacturingComponent } from './manufacturing/manufacturing.component';
+import { TransportationComponent } from './transportation/transportation.component';
+import { UsageComponent } from './usage/usage.component';
+import { DisposalComponent } from './disposal/disposal.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    MaterialsComponent,
+    ManufacturingComponent,
+    TransportationComponent,
+    UsageComponent,
+    DisposalComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/disposal/disposal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disposal',
+  templateUrl: './disposal.component.html',
+  styleUrls: ['./disposal.component.css']
+})
+export class DisposalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
